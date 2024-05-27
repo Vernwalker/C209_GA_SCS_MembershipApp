2cef99fc77d9ac5ee5e656c5b4cdd60ae4f1f38c
Vernwalker <21045050@myrp.edu.sg>
2024-05-27 12:36:58 +0800

    Create module-info.java

---
+++ module-info.java
@@ -0,0 +1,18 @@
+/**
+ * I declare that this code was written by me.
+
+
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * Vernon Ong, 18 Jul 2022 10:46:00 am
+ */
+module GA {
+	exports C209_GA;
+	
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires java.sql;
+	requires java.naming;
+}
